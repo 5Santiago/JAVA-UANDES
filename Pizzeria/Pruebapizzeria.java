e8376bcf7590a6cca081b3363660e297864bef4f
5Santiago <107329231+5Santiago@users.noreply.github.com>
2025-11-13 01:20:31 +0000

    new commit

---
+++ Pizzeria/Pruebapizzeria.java
@@ -0,0 +1,10 @@
+public class Pruebapizzeria {
+    public static void main(String[] args){
+        Pizza napolitana = new Pizza("Napolitana",10000,10,20000);
+        System.out.println(napolitana.darGananciaUnitaria());
+        System.out.println(napolitana.darGananciaTotal());
+
+
+
+    }
+}
